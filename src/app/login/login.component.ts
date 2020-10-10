cf372039121f4ae56b50bf3d953ab2441e5b3c4c
Jardine <jardine.miller@gmail.com>
2020-10-10 16:05:18 +0100

    - Made the login component respect the dark mode preferences

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
 import { Router } from "@angular/router";
+import halfmoon from "halfmoon";
 
 @Component({
     selector: "app-login",
     templateUrl: "./login.component.html",
     styleUrls: ["./login.component.scss"]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
     loginForm: FormGroup;
+    private loading = true;
 
     constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
         this.loginForm = this.formBuilder.group({
@@ -20,13 +22,17 @@ export class LoginComponent implements OnInit {
     }
 
     ngOnInit(): void {
-
+        this.loading = false;
     }
 
     login(): void {
+        this.loading = true;
         this.authService.login(this.loginForm.value).subscribe(response => {
             this.authService.saveToken(response.token);
+            this.loading = false;
             this.router.navigate([""]);
+        }, error => {
+            console.error(error);
         });
     }
 
@@ -37,4 +43,10 @@ export class LoginComponent implements OnInit {
     get password(): AbstractControl {
         return this.loginForm.get("password");
     }
+
+    ngAfterViewInit(): void {
+        halfmoon.onDOMContentLoaded();
+    }
+
+
 }
