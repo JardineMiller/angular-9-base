3de52f2e18d77e1f133be5e0e61e4413d8faf94f
Jardine <jardine.miller@gmail.com>
2020-10-10 16:04:58 +0100

    - Jazzed up the home controller

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, AfterViewInit } from "@angular/core";
 import { AuthService } from "../../services/auth.service";
 import { Router } from "@angular/router";
+import halfmoon from "halfmoon";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
   constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    halfmoon.onDOMContentLoaded();
   }
 
-  logout(): void {
-    this.authService.logout();
-    this.router.navigate(["login"]);
+  toggleSidebar(): void {
+    halfmoon.toggleSidebar();
   }
-
 }
